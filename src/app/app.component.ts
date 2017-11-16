712c5f34ae4aae872327f70b51b12e5d560265d0
BACHTUOCSO\hungdd <hungdd@bachtuocso.vn>
2017-11-16 15:46:36 +0700

    Optomize images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 declare var $: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+    title = 'app';
 
-  ngOnInit() {
-          // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            let target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top)
-                }, 1000, 'easeInOutExpo');
-                return false;
+    ngOnInit() {
+        // Smooth scrolling using jQuery easing
+        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                let target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: (target.offset().top)
+                    }, 1000, 'easeInOutExpo');
+                    return false;
+                }
             }
-        }
-    });
+        });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
+        $('html, body').on('DOMMouseScroll mousewheel', function (event) {
+            if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0) {
+                // scroll down
+                console.log('Down in ');
+            } else {
+                // scroll up
+                console.log('Up');
+            }
+            // prevent page fom scrolling
+            // return false;
+        });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#sideNav'
-    });
-  }
+        // Closes responsive menu when a scroll trigger link is clicked
+        $('.js-scroll-trigger').click(function () {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+        // Activate scrollspy to add active class to navbar items on scroll
+        $('body').scrollspy({
+            target: '#sideNav'
+        });
+    }
 }
