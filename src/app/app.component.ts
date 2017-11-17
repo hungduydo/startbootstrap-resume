cba4e60262f4afa9ea3649c31dca88058e6efc45
BACHTUOCSO\hungdd <hungdd@bachtuocso.vn>
2017-11-17 08:31:58 +0700

    Test load app asyn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,39 +10,39 @@ export class AppComponent implements OnInit {
 
     ngOnInit() {
         // Smooth scrolling using jQuery easing
-        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                let target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: (target.offset().top)
-                    }, 1000, 'easeInOutExpo');
-                    return false;
-                }
-            }
-        });
+        // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        //         let target = $(this.hash);
+        //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        //         if (target.length) {
+        //             $('html, body').animate({
+        //                 scrollTop: (target.offset().top)
+        //             }, 1000, 'easeInOutExpo');
+        //             return false;
+        //         }
+        //     }
+        // });
 
-        $('html, body').on('DOMMouseScroll mousewheel', function (event) {
-            if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0) {
-                // scroll down
-                console.log('Down in ');
-            } else {
-                // scroll up
-                console.log('Up');
-            }
-            // prevent page fom scrolling
-            // return false;
-        });
+        // $('html, body').on('DOMMouseScroll mousewheel', function (event) {
+        //     if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0) {
+        //         // scroll down
+        //         console.log('Down in ');
+        //     } else {
+        //         // scroll up
+        //         console.log('Up');
+        //     }
+        //     // prevent page fom scrolling
+        //     // return false;
+        // });
 
-        // Closes responsive menu when a scroll trigger link is clicked
-        $('.js-scroll-trigger').click(function () {
-            $('.navbar-collapse').collapse('hide');
-        });
+        // // Closes responsive menu when a scroll trigger link is clicked
+        // $('.js-scroll-trigger').click(function () {
+        //     $('.navbar-collapse').collapse('hide');
+        // });
 
-        // Activate scrollspy to add active class to navbar items on scroll
-        $('body').scrollspy({
-            target: '#sideNav'
-        });
+        // // Activate scrollspy to add active class to navbar items on scroll
+        // $('body').scrollspy({
+        //     target: '#sideNav'
+        // });
     }
 }
